ad7252622f3b094c3e5bcc27648f5d1d91cd3db4
3pantz <jintianen07@gmail.com>
2024-10-22 11:42:38 -0400

    Wrote encode and main

---
+++ version_control.py
@@ -0,0 +1,28 @@
+#Timothy Jin
+def encode(password):
+    pass_list = list(password)
+    encode_str = ""
+    for x in pass_list:
+        encode_str += str(int(x)+3)
+    return encode_str
+
+def decode(password):
+    pass
+
+def main():
+    while True:
+        print("Main\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        print()
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+            password = encode(password)
+        elif option == 2:
+            pass
+        elif option == 3:
+            break
+
+if __name__ == '__main__':
+    main()

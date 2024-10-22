f26a32158fb697b855eb2176239a879b58dc2ce9
3pantz <jintianen07@gmail.com>
2024-10-22 14:35:33 -0400

    Wrote encode and main

--- version_control.py
+++ version_control.py
@@ -1,4 +1,5 @@
 #Timothy Jin
+#encode function
 def encode(password):
     pass_list = list(password)
     encode_str = ""
@@ -7,18 +8,20 @@ def encode(password):
     return encode_str
 
 #Sephora Pierre-Louis
+#decode function
 def decode(password):
     password_list = list(password)
     encoded_list = []
     for item in password_list:
         item = int(item)
-        item -= 3
+        item = (item + 7) % 10
         encoded_list.append(str(item))
 
     encoded_string = ''.join(encoded_list)
 
     return encoded_string
 
+#main function
 def main():
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
